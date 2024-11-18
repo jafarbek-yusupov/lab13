42bedb364751c8d2ac1362c21b8c169a5868aa84
j.yusupov <j.yusupov@newuu.uz>
2024-11-18 12:14:52 +0500

    LOMEIN

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#define fl(i, a, b) for(int i = a; i<b; ++i)
+#define fli(i, a, b) for(int i = a; i<=b; ++i)
+#define f(i, a) fl(i, 0,a)
+#define fi(i, a) fli(i,0,a)
+#define all(s) s.begin(), s.end()
+using namespace std;
+
+int main(){
+    return 0;
+}
