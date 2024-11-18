7dad7d733830e177cadd197b4df4e7326fe6a177
j.yusupov <j.yusupov@newuu.uz>
2024-11-18 12:52:49 +0500

    aaaaaaaaa

--- main.cpp
+++ main.cpp
@@ -7,6 +7,14 @@
 #define all(s) s.begin(), s.end()
 using namespace std;
 
+void problem2(){
+    int* ptr;
+    int a[10] = {1,2,3,4,5,6,7,8,9};
+    ptr = a;
+    f(i,9){    cout << *ptr++ << endl;}
+}
+
 int main(){
+    problem2();
     return 0;
 }
